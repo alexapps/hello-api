e8f7de89958da923b8fae018593d2fd2bbb40cf9
alexapps <trim.lexx@gmail.com>
2023-10-17 22:28:23 +0300

    tests added

---
+++ translation/translation_test.go
@@ -0,0 +1,49 @@
+package translation_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/alexapps/hello-api/translation"
+)
+
+func TestTranslate(t *testing.T) {
+	// Arrange
+	// Arrange
+	tt := []struct {
+		Word        string
+		Language    string
+		Translation string
+	}{
+		{
+			Word:        "hallo",
+			Language:    "English",
+			Translation: "hello",
+		},
+		{
+			Word:        "hello",
+			Language:    "german",
+			Translation: "hallo",
+		},
+		{
+			Word:        "hello",
+			Language:    "finnish",
+			Translation: "hei",
+		},
+		{
+			Word:        "hello",
+			Language:    "dutch",
+			Translation: "",
+		},
+	}
+
+	for _, test := range tt {
+		// Act
+		res := translation.Translate(test.Word, test.Language)
+
+		// Assert
+		assert.Equal(t, res, test.Translation, "they should be equal")
+
+	}
+}
